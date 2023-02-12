f69cae0f0eb595b304b5c9f7b044ef35062aec5e
Дмитрий <113688007+DmitryBalakin54@users.noreply.github.com>
2023-02-12 20:39:51 +0300

    Delete package-info.java

--- Java/Markup/markup/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * Tests for <a href="https://www.kgeorgiy.info/courses/prog-intro/homeworks.html#markup">Markup</a> homework
- * of <a href="https://www.kgeorgiy.info/courses/prog-intro/">Introduction to Programming</a> course.
- *
- * @author Georgiy Korneev (kgeorgiy@kgeorgiy.info)
- */
-package markup;
