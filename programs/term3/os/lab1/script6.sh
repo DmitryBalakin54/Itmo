d3890f36e5876512c0e8a281cf8b4902df6adfbc
Дмитрий <113688007+DmitryBalakin54@users.noreply.github.com>
2023-09-30 16:15:11 +0300

    Update script6.sh

--- programs/term3/os/lab1/script6.sh
+++ programs/term3/os/lab1/script6.sh
@@ -5,6 +5,6 @@ touch full.log
 file_out=full.log
 file_in=/var/log/anaconda/X.log
 
-awk -F ' ' '$3=="(WW)"' $file_in | sed 's/(WW)/Warning/g' > $file_out
-awk -F ' ' '$3=="(II)"' $file_in | sed 's/(II)/Information/g' > $file_out
+awk -F ' ' '$3=="(WW)"' $file_in | sed 's/(WW)/Warning:/' > $file_out
+awk -F ' ' '$3=="(II)"' $file_in | sed 's/(II)/Information:/' > $file_out
 
