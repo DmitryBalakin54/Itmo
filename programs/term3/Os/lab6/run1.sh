c453ed7def06796ef8b428c30bff70959efac227
root <root@localhost.localdomain>
2024-01-05 23:53:12 +0300

    dfjkdjf

--- programs/term3/Os/lab6/run1.sh
+++ programs/term3/Os/lab6/run1.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-
+echo "" > "./res1.log"
 for ((N = 1; N <= 20; N++)); do
+    echo "$N" >> "./res1.log"
     for ((i = 1; i <= 10; i++)); do
-        /usr/bin/time -o "./res1.log" -a -f "%e" "test1.sh" $N
+        /usr/bin/time -o "./res1.log" -a -f "%e" "./test1.sh" $N
     done
+    echo "" >> "./res1.log"
 done
