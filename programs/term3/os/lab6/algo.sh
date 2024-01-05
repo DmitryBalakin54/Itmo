6adce5a51fd3f4b39ab9433c18b16af9f706b41b
DmitryBalakin <dmitry54@DESKTOP-S8JKGS7>
2024-01-05 22:59:24 +0300

    os lab6.0

---
+++ programs/term3/os/lab6/algo.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+calculate_sum_of_squares() {
+    local n=$1
+    local sum=0
+
+    for ((i = 1; i <= n; i++)); do
+        square=$((i * i))
+        sum=$((sum + square))
+    done
+
+    echo "$sum"
+}
+
+N=$1
+
+result=$(calculate_sum_of_squares "$N")
+echo "Результат вычисления суммы квадратов от 1 до $N: $result"
