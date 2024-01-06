20ff03056679625c0072d576a236586f835fafa9
root <root@localhost.localdomain>
2024-01-06 13:41:12 +0300

    fkdkfkdjwmd

--- programs/term3/Os/lab6/run2.sh
+++ programs/term3/Os/lab6/run2.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-
-ALGO_RUNNER="./test2.sh"
-OUTPUT_FILE="./res2.log"
-: > "./res2.log"
+echo "" > "./res2.log"
 for ((N = 1; N <= 20; N++)); do
-    echo "" >> "$OUTPUT_FILE"
-    echo "$N" >> "$OUTPUT_FILE"
+    echo "$N" >> "./res2.log"
     for ((i = 1; i <= 10; i++)); do
-        { time $ALGO_RUNNER $N; } 2>&1 | grep "real" | awk '{print $2}' | cut -c 3- | rev | cut -c 2- | rev  >> "$OUTPUT_FILE"
+        /usr/bin/time -o "./res2.log" -a -f "%e" "./test2.sh" $N
     done
+    echo "" >> "./res2.log"
 done
