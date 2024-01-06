2915394f4c3b0daee796da92487ff854b32db2d3
root <root@localhost.localdomain>
2024-01-06 16:09:50 +0300

    dfkdkjdskkfsokdsl

--- programs/term3/Os/lab6/run1_mem.sh
+++ programs/term3/Os/lab6/run1_mem.sh
@@ -4,6 +4,7 @@ N=20
 report="res1_mem.log"
 : > $report
 for run in $(seq $N); do
+        echo "" >> $report
 	echo "$run" >> $report
 	for run2 in $(seq 10); do
 		/usr/bin/time -o "$report" -a -f "%e" "./test1_mem.bash" $run
--- programs/term3/Os/lab6/run2_mem.sh
+++ programs/term3/Os/lab6/run2_mem.sh
@@ -4,6 +4,7 @@ N=20
 report="res2_mem.log"
 : > $report
 for run in $(seq $N); do
+        echo "" >> $report
 	echo "$run" >> $report
 	for run2 in $(seq 10); do
 		/usr/bin/time -o "$report" -a -f "%e" "./test2_mem.bash" $run
