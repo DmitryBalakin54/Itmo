bc9f8ad357e4465c277869d2b67b2aad2dc81573
DmitryBalakin <nursultan54@yandex.ru>
2024-09-03 20:30:50 +0300

    update

--- programs/term2/Algo/4A/A4.java
+++
@@ -1,9 +0,0 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-public class A4 {
-    public static void main(String[] args) throws IOException {
-
-    }
-}
